f02128f780128df62e8afaca56fb175da00d2d0a
Gaurav Singh <25267514+gauraws@users.noreply.github.com>
2022-01-23 16:49:33 +0530

    Clean cabRoutes.js

--- routes/cabRoutes.js
+++ routes/cabRoutes.js
@@ -5,11 +5,5 @@ import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.route('/search').get(protect, getAllCabs);
-// router.route('/track/:bookingId').get(protect, trackRide);
-
-// router
-//   .route('/profile') // protect this routes using jwt token
-//   .get(protect, getUserProfile) // Get user profile
-//   .put(protect, updateUserProfile); // Update user profile
 
 export default router;
